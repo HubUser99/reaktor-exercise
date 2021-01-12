affd1e08d5a6006a9654649a3a898194f72d7335
HubUser99 <cliff.goga@gmail.com>
2021-01-12 22:40:58 +0200

    Added fetching and caching of data from the external api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,51 @@
-import express from "express";
+import express, { RequestHandler } from "express";
+import NodeCache from "node-cache";
+import { getProductsWithAvailabilities } from "utils/api/ApiInterfaceAdapter";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 3001;
+const CACHE_INTERVAL = 300;
+
+const productsCache = new NodeCache({
+    stdTTL: CACHE_INTERVAL,
+    checkperiod: CACHE_INTERVAL,
+});
+
+const updateCache = async () => {
+    console.log("Updating cache...");
+    
+    const products = await getProductsWithAvailabilities();
+    return productsCache.set("products", products);
+};
+
+const cache = (): RequestHandler => {
+    return async (req, res, next) => {
+        const cachedBody = productsCache.get("products");
+
+        if (cachedBody) {
+            next();
+        } else {
+            await updateCache();
+            console.log("Updated cache!");
+            
+            next();
+        }
+    };
+};
 
 app.get("/", (req, res) => {
-    res.send("Hi!");
+    res.send("Hi! This is a caching server for warehouse-explorer.");
+});
+
+app.get("/products", cache(), (req, res) => {
+    const cachedBody = productsCache.get("products");
+
+    console.log("Sending response...");
+    
+    res.send(cachedBody);
 });
 
 app.listen(port, () => {
---
+++ server/src/utils/common/availabilitiesUtils.ts
@@ -0,0 +1,7 @@
+import { AvailabilitiesType, QuantityType } from "types/types";
+
+export const parseAvailabilityDataPayload = (payload: string): QuantityType => {
+    const result = payload.split("<INSTOCKVALUE>")[1].split("</INSTOCKVALUE>")[0] as QuantityType;
+
+    return result;
+};
